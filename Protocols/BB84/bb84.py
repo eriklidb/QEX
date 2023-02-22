0baa142a5afd3196c32290547e94a55ef3587669
Erik <eriklidb@kth.se>
2023-02-22 15:35:03 +0100

    Integrated updated circuit and rke and key.

--- Protocols/BB84/bb84.py
+++ Protocols/BB84/bb84.py
@@ -1,65 +1,65 @@
 import numpy as np
 
-from qiskit import QuantumCircuit, transpile
+from qiskit import QuantumCircuit, transpile, QuantumRegister, ClassicalRegister
 from qiskit.providers.aer import QasmSimulator
 from qiskit.visualization import plot_histogram
 
-numIters = 5 
-debug = False
-senderBasis = []
-senderBits = []
-receiverBasis = []
-receiverBits = []
+num_iters = 10 
+sender_basis = np.empty(num_iters, dtype='<U1')
+sender_bits = np.empty(num_iters, dtype='<i4')
+receiver_basis = np.empty(num_iters, dtype='<U1')
+receiver_bits = np.empty(num_iters, dtype='<i4')
 
-for i in range(numIters):
-    # Use Aer's qasm_simulator
-    simulator = QasmSimulator()
+simulator = QasmSimulator()
+for i in range(num_iters):
+    qreg_q = QuantumRegister(1, 'q')
+    creg_r_bit = ClassicalRegister(1, 'r_bit')
+    creg_s_bit = ClassicalRegister(1, 's_bit')
+    creg_r_bas = ClassicalRegister(1, 'r_bas')
+    creg_s_bas = ClassicalRegister(1, 's_bas')
+    circuit = QuantumCircuit(qreg_q, creg_r_bit, creg_s_bit, creg_r_bas, creg_s_bas)
 
-    # Create a Quantum Circuit acting on the q register
-    circuit = QuantumCircuit(1, 1)
+    circuit.h(qreg_q[0])
+    circuit.measure(qreg_q[0], creg_s_bas[0])
+    circuit.h(qreg_q[0])
+    circuit.measure(qreg_q[0], creg_r_bas[0])
+    circuit.barrier(qreg_q[0])
+    circuit.h(qreg_q[0])
+    circuit.measure(qreg_q[0], creg_s_bit[0])
+    circuit.h(qreg_q[0]).c_if(creg_s_bas, 1)
+    circuit.barrier(qreg_q[0])
+    circuit.h(qreg_q[0]).c_if(creg_r_bas, 1)
+    circuit.measure(qreg_q[0], creg_r_bit[0])
 
-    senderBits.append(0)
-    senderUseX = np.random.choice([False, True])
-    if senderUseX:
-        circuit.x(0)
-        senderBits[i] = 1
+    compiled_circuit = transpile(circuit, simulator)
+    job = simulator.run(compiled_circuit, shots=1)
+    result = job.result()
+    counts_arr = np.array(list(result.get_counts().keys())[0].split(), dtype='<i4')
 
-    senderUseH = np.random.choice([False, True])
-    if senderUseH:
-        circuit.h(0)
-        senderBasis.append('X')
-    else:
-        senderBasis.append('Z')
+    sender_basis[i] = '+' if counts_arr[3] == 0 else 'X'
+    sender_bits[i] = counts_arr[1]
+    receiver_basis[i] = '+' if counts_arr[2] == 0 else 'X'
+    receiver_bits[i] = counts_arr[0]
 
-    receiverUseH = np.random.choice([False, True])
-    if receiverUseH:
-        circuit.h(0)
-        receiverBasis.append('X')
-    else:
-        receiverBasis.append('Z')
-    circuit.measure([0], [0])
+rke = (sender_basis == receiver_basis).mean()
+key = sender_bits[np.where((sender_basis == receiver_basis) == True)[0]]
+
+print(sender_basis[:10])
+print(sender_bits[:10])
+print(receiver_basis[:10])
+print(receiver_bits[:10])
+print(rke)
+print(key)
 
     # compile the circuit down to low-level QASM instructions
     # supported by the backend (not needed for simple circuits)
-    compiled_circuit = transpile(circuit, simulator)
+    #compiled_circuit = transpile(circuit, simulator)
 
     # Execute the circuit on the qasm simulator
-    job = simulator.run(compiled_circuit, shots=1)
+    #job = simulator.run(compiled_circuit, shots=1)
 
     # Grab results from the job
-    result = job.result()
-    for key in result.get_counts().keys():
-        receiverBits.append(int(key))
+    #result = job.result()
+    #for key in result.get_counts().keys():
+    #    receiverBits.append(int(key))
 
-    # Returns counts
-    if debug:
-        print("Sender use X = ", senderUseX)
-        print("Sender use H = ", senderUseH)
-        print("Receiver use H = ", receiverUseH)
-        #print("Sender classical bits = ", senderBit)
-        print("Receiver classical bits = ", result.get_counts())
-
-print(senderBasis)
-print(senderBits)
-print(receiverBasis)
-print(receiverBits)
