bf68755af5138fd389d0b61bca32d76f4b575835
nylanderdev <rasmus.s.nylander@gmail.com>
2023-02-22 15:55:24 +0100

    Reverse bit order

--- Protocols/BB84/bb84.py
+++ Protocols/BB84/bb84.py
@@ -4,7 +4,7 @@ from qiskit import QuantumCircuit, transpile, QuantumRegister, ClassicalRegister
 from qiskit.providers.aer import QasmSimulator
 from qiskit.visualization import plot_histogram
 
-num_iters = 10 
+num_iters = 10
 sender_basis = np.empty(num_iters, dtype='<U1')
 sender_bits = np.empty(num_iters, dtype='<i4')
 receiver_basis = np.empty(num_iters, dtype='<U1')
@@ -35,21 +35,23 @@ for i in range(num_iters):
     job = simulator.run(compiled_circuit, shots=1)
     result = job.result()
     counts_arr = np.array(list(result.get_counts().keys())[0].split(), dtype='<i4')
-
-    sender_basis[i] = '+' if counts_arr[3] == 0 else 'X'
-    sender_bits[i] = counts_arr[1]
-    receiver_basis[i] = '+' if counts_arr[2] == 0 else 'X'
-    receiver_bits[i] = counts_arr[0]
+    
+    sender_basis[i] = '+' if counts_arr[0] == 0 else 'X'
+    sender_bits[i] = counts_arr[2]
+    receiver_basis[i] = '+' if counts_arr[1] == 0 else 'X'
+    receiver_bits[i] = counts_arr[3]
 
 rke = (sender_basis == receiver_basis).mean()
 key = sender_bits[np.where((sender_basis == receiver_basis) == True)[0]]
+rkey = receiver_bits[np.where((sender_basis == receiver_basis) == True)[0]]
 
-print(sender_basis[:10])
-print(sender_bits[:10])
-print(receiver_basis[:10])
-print(receiver_bits[:10])
-print(rke)
-print(key)
+print("S Basis\t", sender_basis)
+print("S Bits\t", sender_bits)
+print("R Basis\t", receiver_basis)
+print("R Bits\t", receiver_bits)
+print("Basis match rate\t", rke)
+print("S Key\t", key)
+print("R Key\t", rkey)
 
     # compile the circuit down to low-level QASM instructions
     # supported by the backend (not needed for simple circuits)
